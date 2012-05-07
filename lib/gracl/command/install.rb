7dcf8d2a29f6074821ca6acb48bca1fdf0a0b739
Richard Clamp <richardc@unixbeard.net>
2012-05-07 19:56:33 +0100

    drop the ssh config in place with setup

--- lib/gracl/command/install.rb
+++ lib/gracl/command/install.rb
@@ -1,8 +1,8 @@
 class Gracl::Command::Install < Gracl::Command
     parameter "ADMIN", "public keyfile of the initial administrator"
     def execute
-        gracl = Gracl.new(config)
+        gracl = Gracl.new(File.expand_path("~/.gracl"))
         gracl.create_admin_repository(admin)
-        #gracl.install_ssh_config
+        gracl.install_ssh_config
     end
 end
--- lib/gracl/command/setup.rb
+++ lib/gracl/command/setup.rb
@@ -1,7 +1,11 @@
 class Gracl::Command::Setup < Gracl::Command
     def execute
         config = Gracl::Config.new(File.expand_path("~/.gracl"))
-        config.validate
-        puts config.to_yaml
+        if !config.validate
+            raise "config fails to validate, not asserting"
+        end
+
+        gracl = Gracl.new((File.expand_path("~/.gracl")))
+        gracl.install_ssh_config
     end
 end
