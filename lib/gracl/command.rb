f547ff81cb02e49d4cdc8693ed58fd71adcc122f
Richard Clamp <richardc@unixbeard.net>
2012-05-07 19:37:22 +0100

    when -v, enable Grit debug

--- lib/gracl/command.rb
+++ lib/gracl/command.rb
@@ -1,5 +1,7 @@
 class Gracl::Command < Clamp::Command
-    option ["-v", "--verbose"], :flag, "be verbose"
+    option ["-v", "--verbose"], :flag, "be verbose" do
+        Grit.debug = true
+    end
     option ["-c", "--config"], "CONFIG", "configuration directory", :default => "."
     subcommand "shell",   "The shell invoked from SSH", Gracl::Command::Shell
     subcommand "install", "Install Gracl",              Gracl::Command::Install
