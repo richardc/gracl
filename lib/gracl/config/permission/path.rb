e3766f7dd2b6518187d89b0488584d29f96766d1
Richard Clamp <richardc@unixbeard.net>
2012-05-13 16:26:34 +0100

    path-based
    This is the most complex, as we have to examine the commit to see
    what files are getting changed

--- lib/gracl/config/permission/path.rb
+++ lib/gracl/config/permission/path.rb
@@ -10,6 +10,35 @@ class Gracl::Config::Permission::Path < Gracl::Config::Permission
         true
     end
 
+    def allow?(repo, ref, oldsha, newsha)
+        empty_tree = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+        oldtree = oldsha == "0" * 40 ? empty_tree : oldsha
+        newtree = newsha == "0" * 40 ? empty_tree : newsha
+
+        diff = POSIX::Spawn::Child.new('git', 'diff', '--name-only', oldtree, newtree)
+        changed = diff.out.split(/\n/)
+        changed.each do |file|
+            if !matches? "/#{file}"
+                # XXX log file matched doesn't equal path
+                return false
+            end
+        end
+
+        true
+    end
+
+    def matches?(file)
+        case path.class
+        when String
+            File.fnmatch(file, path)
+        when Regexp
+            file =~ path
+        else
+            # XXX log invalid path
+            false
+        end
+    end
+
     def describe
         "allow_path #{path.inspect} => #{who.inspect}"
     end
